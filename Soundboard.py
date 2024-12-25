1c65400ab11f073aa788e8700669a7ba1a710c3b
Shashank Jayakumar <60236335+shayshunk@users.noreply.github.com>
2024-12-24 23:48:36 -1000

    Adding sound now makes new button associated with the sound. Channel configuration still needs work but it creates a new button in the grid.

--- Soundboard.py
+++ Soundboard.py
@@ -1,33 +1,58 @@
 import tkinter as tk
 import customtkinter as ctk
 from pygame import mixer
+from math import floor
 
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("blue")
 
 mixer.init()
-mixer.set_num_channels(8)
+totalChannels = 8
+mixer.set_num_channels(totalChannels)
+
+columns = 2
 
 
-def AddSound():
-    soundFile = tk.filedialog.askopenfilename()
-    newChannel = mixer.find_channel(force=True)
-    print(soundFile)
+def PlaySound(buttonId):
+    soundFile = soundDictionary[buttonId]
     sound = mixer.Sound(soundFile)
+    newChannel = mixer.find_channel(force=True)
     newChannel.play(sound)
 
 
+def AddSound(buttonId):
+    # Asking user to associate file with button
+    soundFile = tk.filedialog.askopenfilename()
+
+    # Associating sound with dictionary
+    soundDictionary[buttonId] = soundFile
+    buttons[buttonId].configure(command=lambda: PlaySound(buttonId))
+
+
 def AddButton(app):
+    # Assigning new button to dictionary
+    buttonId = str(len(buttons) + 1)
+
+    # Figuring out where to place new button
+    totalButtons = len(buttons)
+    columnSpot = totalButtons % columns
+    rowSpot = floor(totalButtons / 2) + 1
+
     # Creating new button for sound
-    newButton = ctk.CTkButton(master=app, text="", command=AddSound())
-    newButton.configure(width=10, height=50)
-    newButton.grid(row=1, column=0, padx=20, pady=20)
+    buttons[buttonId] = ctk.CTkButton(master=app, text="")
+    buttons[buttonId].configure(command=AddSound(buttonId))
+    buttons[buttonId].configure(width=150, height=100)
+    buttons[buttonId].grid(row=rowSpot, column=columnSpot,
+                           padx=20, pady=20, sticky="ew")
 
     # Adding entry box to get name for sound
     dialog = ctk.CTkInputDialog(
         text="Enter sound name:", title="Name your buton!")
     buttonName = dialog.get_input()
-    newButton.configure(text=buttonName)
+    buttons[buttonId].configure(text=buttonName)
+
+    # Changing what clicking the button does
+    buttons[buttonId].configure(command=lambda: PlaySound(buttonId))
 
 
 # Creating app
@@ -38,10 +63,13 @@ app.title("Soundboard")
 app.grid_columnconfigure(0, weight=1)
 app.grid_columnconfigure(1, weight=1)
 
+buttons = {}
+soundDictionary = {}
+
 label = ctk.CTkLabel(master=app, text="Soundboard")
 label.grid(row=0, column=0, padx=20, pady=20, sticky='ew')
 
-button = ctk.CTkButton(master=app, text="Play Sound",
+button = ctk.CTkButton(master=app, text="Add Sound",
                        command=lambda: AddButton(app))
 button.grid(row=0, column=1, padx=20, pady=20, sticky='ew')
 
