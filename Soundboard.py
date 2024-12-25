7b789a4a7e401100cf4bfb4e74b0be1957f374d4
Shashank Jayakumar <60236335+shayshunk@users.noreply.github.com>
2024-12-24 21:10:12 -1000

    Moving to custom Tkinter

--- Soundboard.py
+++ Soundboard.py
@@ -1,23 +1,26 @@
 import tkinter as tk
-import ttkbootstrap as ttk
+import customtkinter
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
 
 
 def AddButton(window):
-    newButton = ttk.Button(master=window, text='Added')
+    newButton = customtkinter.CTkButton(master=window, text='Added')
     newButton.pack(pady=10)
 
 
 # Creating window
-window = ttk.Window(themename='solar')
+window = customtkinter.CTk()
 
 window.geometry("800x800")
 window.title("Soundboard")
 
-label = ttk.Label(master=window, text="Soundboard")
+label = customtkinter.CTkLabel(master=window, text="Soundboard")
 label.pack(pady=20)
 
-button = ttk.Button(master=window, text="Add Sound",
-                    command=lambda: AddButton(window))
+button = customtkinter.CTkButton(master=window, text="Add Sound",
+                                 command=lambda: AddButton(window))
 button.pack()
 
 # Running
